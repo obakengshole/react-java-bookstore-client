2875eae7e2b801bde0bc55b70b0a1ecc038c6caa
obakengshole <shole.obakeng@gmail.com>
2021-08-17 19:04:36 +0200

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
     axios(`${baseUrl}/api/v1/books`).then(books => {
         console.log(books);
     })
-    return <div>My first component</div>
+    return <div>My first component updated</div>
 }
 
 export default App;
