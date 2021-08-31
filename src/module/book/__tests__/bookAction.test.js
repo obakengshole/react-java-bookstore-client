422355826426709015f45808cdfb6702ab3ba8f8
obakengshole <shole.obakeng@gmail.com>
2021-08-31 14:42:02 +0200

    Added getBooksByTitle unit tests

--- src/module/book/__tests__/bookAction.test.js
+++ src/module/book/__tests__/bookAction.test.js
@@ -1,7 +1,7 @@
 import configureStore from 'redux-mock-store';
 import reduxThunk from 'redux-thunk';
 import axios from 'axios';
-import { getBooksAction } from '../bookAction';
+import { getBooksAction, getBooksByTitle } from '../bookAction';
 
 jest.mock('axios');
 const middleware = [reduxThunk];
@@ -27,8 +27,8 @@ describe('BookActions', () => {
     const store = mockStore({});
     await store.dispatch(getBooksAction());
     const actions = store.getActions();
-    expect(actions.length).toEqual(1);
-    expect(actions[0]).toEqual({
+    expect(actions.length).toEqual(3);
+    expect(actions[1]).toEqual({
       type: 'BOOKLIST',
       payload: [
         {
@@ -39,5 +39,39 @@ describe('BookActions', () => {
         },
       ]
     });
+  })
+
+  it('should be able to dispatch bookbytitle action', async() => {
+    const store = mockStore({})
+    await store.dispatch(getBooksByTitle('test title'))
+
+    const actions = store.getActions()
+
+    expect(actions.length).toEqual(3)
+    expect(actions(1)).toEqual({
+      type: 'BOOKSBYTITLE',
+      payload: [{
+        id: 1,
+          title: 'test title',
+          description: 'des',
+          releaseYear: 2018
+      }]
+    })
+  })
+
+  it('should be able to dispatch error action', async() => {
+    const store = mockStore({})
+    axios.get.mockImplementation(() => {
+      throw new Error()
+    })
+
+    await store.dispatch(getBooksByTitle('test title'))
+    
+    const actions = store.getActions()
+
+    expect(actions.length).toEqual(2)
+    expect(actions[1]).toEqual({
+      type: 'BOOKLISTERROR'
+    })
   })
 })
--- src/module/book/__tests__/bookReducer.test.js
+++ src/module/book/__tests__/bookReducer.test.js
@@ -18,7 +18,31 @@ describe('Book reducer', () => {
                 title: 'test title',
                 description: 'des',
                 releaseYear: 2018
-            }]
+            }],
+            promise: { isPending: false, isFulfilled: false, isErrorOccurred: false}
           })
+    })
+
+    it('should return new state for BOOKSBYTITLE action', () => {
+        const action = {
+            type: 'BOOKSBYTITLE',
+            payload: [{
+                id: 1,
+                title: 'another book',
+                description: 'another description',
+                releaseYear: 2018
+            }]
+        }
+
+        const newState = bookReducer(INITIAL_BOOK_STATE, action)
+        expect(newState).toEqual({
+            books: [{
+                id: 1,
+                title: 'another book',
+                description: 'another description',
+                releaseYear: 2018
+            }],
+            promise: { isPending: false, isFulfilled: false, isErrorOccurred: false}
+        })
     })
 })
