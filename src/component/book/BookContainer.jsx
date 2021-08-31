50e3b6038f824e7f8f7624cefa56fcfdc2091855
obakengshole <shole.obakeng@gmail.com>
2021-08-31 14:53:22 +0200

    Fixed ui issues

--- src/component/book/BookContainer.jsx
+++ src/component/book/BookContainer.jsx
@@ -4,8 +4,9 @@ import BookFilter from './BookFilter'
 import styles from './BookStyles'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBooksAction } from '../../module/book/bookAction'
-import { getBooksSelector } from '../../module/book/bookSelector'
+import { getBooksSelector, getBookPromiseSelector } from '../../module/book/bookSelector'
 import BookList from './BookList'
+import { Skeleton } from '@material-ui/lab'
 
 const BookContainer = () => {
     const dispatch = useDispatch() // will provide dispatch function through which we can dispatch our action
@@ -15,13 +16,20 @@ const BookContainer = () => {
     }, [dispatch])
 
     const books = useSelector(getBooksSelector)
+    const bookPromise = useSelector(getBookPromiseSelector)
     // console.log(books);
     const classes = styles()
     return (
         <Box className={classes.bookContainer}>
             <BookFilter />
             <Box className={classes.bookList}>
-                <BookList books={books}/>
+                { bookPromise.isPending && (
+                    <Box ml={5}>
+                        <Skeleton data-testid="book-loader" variant="rect" animation="pulse" width="80%" height={200}/>
+                    </Box>
+                )}
+                { bookPromise.isErrorOccurred && (<div data-testid="book-error-message">Error message ...</div> )}
+                { <BookList books={books} /> }
             </Box>
         </Box>
     )
